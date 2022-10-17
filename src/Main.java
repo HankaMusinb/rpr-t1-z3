6e99634ca7922e0eb37a6ae114709f7e4bc14de6
hmusinbego1 <hmusinbego1@etf.unsa.ba>
2022-10-17 10:17:02 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        Sat s = new Sat(15,20, 23);
+        s.Ispisi();
+        s.Sljedeci();
+        s.Ispisi();
+        s.PomjeriZa(-48);
+        s.Ispisi();
+        s.Postavi(0,0,0);
+        s.Ispisi();
+
+    }
+}
